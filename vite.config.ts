532e15141fba3ab80fcc293960ec38489f863cf9
yao19992021@outlook.com <yao19992021@outlook.com>
2025-05-19 12:06:23 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // 监听所有 IP 地址
+    port: 3001, // 你可以选择一个合适的端口
+  },
+});
