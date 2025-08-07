608ef378fd7241d6f9a76ce464ca5ea3d4e516e7
yao19992021@outlook.com <yao19992021@outlook.com>
2025-08-07 13:08:07 +0800

    commit

--- src/pages/dashboard/transfer/table/PendingTransferTable.tsx
+++ src/pages/dashboard/transfer/table/PendingTransferTable.tsx
@@ -123,6 +123,14 @@ const PendingTransferTable = ({ pendingTransferRecod, handleGetPendingTransactio
         return record?.isManual === 1 && text === "DONE" ? <Tag color="#13c2c2">MANUAL DONE</Tag> : <Tag color={text === "WAITING" ? "#2db7f5" : text === "HOLD" ? "#ad8b00" : text === "DONE" ? "#87d068" : text === "REJECT" ? "#f50" : text === "TOP UP" ? "#36cfc9" : ""}>{text}</Tag>;
       },
     },
+    {
+      title: t("systemRemark"),
+      dataIndex: "sysRemark1",
+      align: "center",
+      render: (text: string) => {
+        return <div style={{ fontWeight: "600" }}>{formatString(text)}</div>;
+      },
+    },
     // {
     //   title: t("staff"),
     //   dataIndex: "mStaff",
